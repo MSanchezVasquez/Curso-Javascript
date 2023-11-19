ff174e0b252a823280933d00ecaf60b847da39f9
MSanchezVasquez <moisesup.25@gmail.com>
2023-11-19 00:06:25 -0500

    math

---
+++ 16. Objeto Math.js
@@ -0,0 +1,14 @@
+console.log(Math); //Object [Math] {}
+console.log(Math.PI); //3.141592653589793
+console.log(Math.abs(7.8)); //7.8
+console.log(Math.abs(-7.8)); //7.8
+console.log(Math.ceil(7.8)); //8
+console.log(Math.floor(7.8)); //7
+console.log(Math.round(7.49)); //7
+console.log(Math.sqrt(81)); //9
+console.log(Math.pow(2, 5)); //32
+console.log(Math.sign(-9)); //-1 <= Referencia a negativo
+console.log(Math.sign(9)); //1 <=Referencia a positivo
+console.log(Math.random()); //0.10489951942314302
+console.log(Math.random() * 100); //87.2696733349793
+console.log(Math.round(Math.random() * 100)); //22
