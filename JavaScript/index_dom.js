11fd750e3a46725e6a5136ae123dc9be39246d2d
MSanchezVasquez <moisesup.25@gmail.com>
2024-03-05 11:14:26 -0500

    cuenta-regresiva

--- JavaScript/index_dom.js
+++ JavaScript/index_dom.js
@@ -1,6 +1,7 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
 
 const d = document;
 
@@ -8,6 +9,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".panel-btn", ".panel", "menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "Dec 23, 2024 11:59:59", "Feliz Cumpleaños! 🎂");
 });
 
 /* d.addEventListener("keyup", (e) => {
